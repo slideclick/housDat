39af7d0f945d10ad891f5753211411eeba8df3d4
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2016-08-12 17:23:10 +0800

    烂人style居然带有]r\n只好用\x解决

--- bjjsDat/bjjsDat/bjjsDat.py
+++ bjjsDat/bjjsDat/bjjsDat.py
@@ -2,43 +2,14 @@ url = 'http://www.bjjs.gov.cn/tabid/2167/default.aspx'
 import requests
 from bs4 import BeautifulSoup as soup
 import sys
-def get_links(url):
-    import requests
-    from bs4 import BeautifulSoup as soup
-    result = requests.get(url)
-    page = result.text
-    doc = soup(page)
-    links = [element.get('href') for element in doc.find_all('a')] 
-    return links
-
-def get_tables(url):
-    result = requests.get(url)
-    page = result.text
-    doc = soup(page)
-    tables = [element.get('style') for element in doc.find_all('table') if element.get('width') == '969' \
-    and element.get('border')== '0'] 
-    return tables    
     
-def getCunLiangFangTable(url=url):
-    url = 'http://www.bjjs.gov.cn/tabid/2167/default.aspx'
-    result = requests.get(url)
-    page = result.text
-    doc = soup(page)
-    table = doc.find_all('table', attrs={'width': '969' })
-    print(table)    
 
 url = 'http://www.bjjs.gov.cn/tabid/2167/default.aspx'
 result = requests.get(url)
 page = result.text
 doc = soup(page)
-table = doc.find_all('table', attrs={'width': '969' })
-print(table)   
+tables = doc.find_all('table', attrs={'width': '969'  ,'cellspacing':"5" ,'cellpadding':"0",  'border':"0"
+,'style':'border-left: #DBBF93 solid 1px;\r\n        border-right: #DBBF93 solid 1px;'})
+print(tables)   
 
     
-if __name__ == '__main__':
-        getCunLiangFangTable()
-        pass
-        print('Links in', url)
-        for num, table in enumerate(get_tables(url), start=1):
-            print(num, table)
-        print()
