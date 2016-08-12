373268f34e074939a1c0d7b7945a687c9562dd10
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2016-08-12 17:04:46 +0800

    get tables

--- bjjsDat/bjjsDat/bjjsDat.py
+++ bjjsDat/bjjsDat/bjjsDat.py
@@ -11,8 +11,34 @@ def get_links(url):
     links = [element.get('href') for element in doc.find_all('a')] 
     return links
 
+def get_tables(url):
+    result = requests.get(url)
+    page = result.text
+    doc = soup(page)
+    tables = [element.get('style') for element in doc.find_all('table') if element.get('width') == '969' \
+    and element.get('border')== '0'] 
+    return tables    
+    
+def getCunLiangFangTable(url=url):
+    url = 'http://www.bjjs.gov.cn/tabid/2167/default.aspx'
+    result = requests.get(url)
+    page = result.text
+    doc = soup(page)
+    table = doc.find_all('table', attrs={'width': '969' })
+    print(table)    
+
+url = 'http://www.bjjs.gov.cn/tabid/2167/default.aspx'
+result = requests.get(url)
+page = result.text
+doc = soup(page)
+table = doc.find_all('table', attrs={'width': '969' })
+print(table)   
+
+    
 if __name__ == '__main__':
+        getCunLiangFangTable()
+        pass
         print('Links in', url)
-        for num, link in enumerate(get_links(url), start=1):
-            print(num, link)
+        for num, table in enumerate(get_tables(url), start=1):
+            print(num, table)
         print()
